0df81c841bd8275e050aa76583299fbebf08d002
Ankur Raina <Ankur10gen@users.noreply.github.com>
2022-12-16 00:15:09 +0800

    added pickAndDeliver function first draft

---
+++ AtlasAppService/functions/pickAndDeliver.js
@@ -0,0 +1,39 @@
+exports = function pickAndDeliver(args) {
+  var sourceColl = context.services
+    .get("federated-instance")
+    .db("federated")
+    .collection("changestreams");
+
+  var milestoneColl = context.services
+    .get("mongodb-atlas")
+    .db("federated")
+    .collection("milestone");
+
+  var prevMilestone = milestoneColl.findOne({}, { ts: 1, batch: 1, _id: 0 });
+  var nextMilestone = prevMilestone.ts + 5 * 60 * 1000; //5 mins into ms
+
+  var agg = [
+    {
+      $match: { clusterTime: { $gte: prevMilestone.ts, $lte: nextMilestone } },
+    },
+    {
+      $out: {
+        s3: {
+          bucket: "ankurs-bizprod-bucket",
+          region: "us-east-1",
+          filename: "BATCH-".concat(prevMilestone.batch),
+          format: {
+            name: "parquet",
+          },
+        },
+      },
+    },
+  ];
+
+  sourceColl.aggregate(agg, { background: true });
+
+  milestoneColl.updateOne(
+    {},
+    { $set: { ts: nextMilestone }, $inc: { batch: "1" } }
+  );
+};
